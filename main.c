b4c905164bfc88695ab25a57c3ec12d5386e5f76
Nicolas Bonfante <nicolas.bonfante@doctolib.com>
2023-11-06 10:30:46 +0100

    Add concat feature

--- main.c
+++ main.c
@@ -3,9 +3,20 @@
 #include <string.h>
 
 #define N 10
+#define MAX 1000
 
-void handleLine(char* line) {
+int concat_idx = 0;
+
+void handleLine(char* line, char* concat) {
     printf("[%s]\n", line);
+
+    memcpy(concat + concat_idx, line, strlen(line));
+    concat_idx += strlen(line);
+}
+
+void handleConcat(char* concat) {
+    printf("[%s]\n", concat);
+    free(concat);
 }
 
 int main() {
@@ -19,11 +30,13 @@ int main() {
     // Read file line by line.
     // Output each line.
     char line[N];
+    char* concat = (char*) malloc(N*MAX);
+    memset(concat, 0x0, N*MAX);
     int i = 0;
     char ch;
     while ((ch = fgetc(file)) != EOF) {
         if (ch == '\n') {
-            handleLine(line);
+            handleLine(line, concat);
             i = 0;
             memset(line, 0x0, N);
             continue;
@@ -32,9 +45,11 @@ int main() {
         line[i] = ch;
         i++;
     }
-    handleLine(line);
+    handleLine(line, concat);
+    handleConcat(concat);
 
     // Cleanup everything.
+    memset(concat, 0x0, N*MAX);
     fclose(file);
 
     return 0;
